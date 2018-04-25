8aa2af7cc03fef0eb428437fd2b6cad7327b4403
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-04-25 13:06:28 -0400

    Added Codes

---
+++ AddressProgramForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace AddressProgramToluA
+{
+    partial class frmAddressProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAptNumber = new System.Windows.Forms.Label();
+            this.lblStreetAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblEnterInformation = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAptNumber
+            // 
+            this.lblAptNumber.AutoSize = true;
+            this.lblAptNumber.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAptNumber.Location = new System.Drawing.Point(12, 54);
+            this.lblAptNumber.Name = "lblAptNumber";
+            this.lblAptNumber.Size = new System.Drawing.Size(103, 16);
+            this.lblAptNumber.TabIndex = 0;
+            this.lblAptNumber.Text = "Apt. Number :";
+            // 
+            // lblStreetAddress
+            // 
+            this.lblStreetAddress.AutoSize = true;
+            this.lblStreetAddress.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreetAddress.Location = new System.Drawing.Point(12, 89);
+            this.lblStreetAddress.Name = "lblStreetAddress";
+            this.lblStreetAddress.Size = new System.Drawing.Size(127, 16);
+            this.lblStreetAddress.TabIndex = 1;
+            this.lblStreetAddress.Text = "Street Address :";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(12, 128);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(46, 16);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City :";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(12, 167);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(84, 16);
+            this.lblProvince.TabIndex = 3;
+            this.lblProvince.Text = "Province :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(120, 167);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(11, 270);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 8;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblEnterInformation
+            // 
+            this.lblEnterInformation.AutoSize = true;
+            this.lblEnterInformation.Font = new System.Drawing.Font("Trajan Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterInformation.Location = new System.Drawing.Point(-2, 9);
+            this.lblEnterInformation.Name = "lblEnterInformation";
+            this.lblEnterInformation.Size = new System.Drawing.Size(196, 20);
+            this.lblEnterInformation.TabIndex = 9;
+            this.lblEnterInformation.Text = "Enter Information :\r\n";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(12, 210);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(107, 16);
+            this.lblPostalCode.TabIndex = 10;
+            this.lblPostalCode.Text = "Postal Code :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(120, 210);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // frmAddressProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 335);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblEnterInformation);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblStreetAddress);
+            this.Controls.Add(this.lblAptNumber);
+            this.Name = "frmAddressProgram";
+            this.Text = "Address Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAptNumber;
+        private System.Windows.Forms.Label lblStreetAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblEnterInformation;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ AddressProgramForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressProgramToluA
+{
+    public partial class frmAddressProgram : Form
+    {
+        public frmAddressProgram()
+        {
+            InitializeComponent();
+        }
+        public void PrintName (String aptNumber, String streetAddress, String city, String province, String postalCode)
+        {
+            MessageBox.Show("Your Address is: " + aptNumber + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+
+        public void PrintName (String streetAddress, String city, String province, String postalCode)
+        {
+            MessageBox.Show("Your Address is: " + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            String userAptNumber, UserStreetAddress, UserCity, UserProvince, userPostalCode;
+        }
+    }
+}
